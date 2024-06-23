19f4e6d7dded1593a418eca2ec080b16ca543163
Emil Deßloch <emildess0303@gymhelm.onmicrosoft.com>
2024-06-23 20:53:16 +0200

    refactor: Update `tensorflow_test no intensity.py` with improved LeakyReLU activation and custom model filename

--- AI/tensorflow_test no intensity.py
+++ AI/tensorflow_test no intensity.py
@@ -2,9 +2,9 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, LeakyReLU
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization, LeakyReLU # type: ignore
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint # type: ignore
 import numpy as np
 import matplotlib.pyplot as plt
 import uuid
