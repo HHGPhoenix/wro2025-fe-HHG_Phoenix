33b29b7aecdfd01a07d873e0ee638ffc6ef1783b
Emil Deßloch <emildess0303@gymhelm.onmicrosoft.com>
2024-06-29 12:45:57 +0200

    feat: Update DataTransferer constructor to include camera and lidar parameters

--- RPIs/DataManager/DataTransferer/DataTransferer.py
+++ RPIs/DataManager/DataTransferer/DataTransferer.py
@@ -1,21 +1,18 @@
 import cv2
 import numpy as np
 from multiprocessing import Manager
-import time
 
 class DataTransferer:
-    def __init__(self, shared_list):
+    def __init__(self, camera, lidar, shared_list):
+        self.camera = camera
+        self.lidar = lidar
         self.shared_list = shared_list
     
     def process_cam_frames(self):
         while True:
-            # Generate random frames (640x480 RGB)
-            frameraw = np.random.randint(0, 256, (480, 640, 3), dtype=np.uint8)
-            simplified_image = np.random.randint(0, 256, (480, 640, 3), dtype=np.uint8)
+            frameraw, framehsv = self.camera.capture_array()
+            simplified_image = self.camera.simplify_image(framehsv, [0, 255, 0], [255, 0, 0])
             
             # Update shared list with the new frames
             self.shared_list[0] = frameraw.tobytes()
             self.shared_list[1] = simplified_image.tobytes()
-            
-            # Simulate frame rate
-            time.sleep(1 / 30)  # 30 frames per second
