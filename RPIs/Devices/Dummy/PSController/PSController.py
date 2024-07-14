93f67ac69140d9d90d400a834fcfde985df36ca8
Emil Deßloch <emildess0303@gymhelm.onmicrosoft.com>
2024-07-14 17:39:21 +0200

    added gyro code

--- RPIs/Devices/Dummy/PSController/PSController.py
+++ RPIs/Devices/Dummy/PSController/PSController.py
@@ -18,50 +18,50 @@ class PSController:
         ry = random.uniform(-1, 1)
         return x, y, rx, ry
 
-def main_loop_training(self):
-    self.logger.info("Starting main loop for training...")
-    ps_controller = PSController()
-    ps_controller.calibrate_analog_sticks()
+# def main_loop_training(self):
+#     self.logger.info("Starting main loop for training...")
+#     ps_controller = PSController()
+#     ps_controller.calibrate_analog_sticks()
     
-    file_uuid = str(uuid4())
-    date = datetime.date.today()
+#     file_uuid = str(uuid4())
+#     date = datetime.date.today()
     
-    recording_status = False
-    cross_pressed = False
+#     recording_status = False
+#     cross_pressed = False
 
-    start_time = time.time()
+#     start_time = time.time()
     
-    while self.running:
-        if ps_controller.cross == 1 and not cross_pressed:
-            cross_pressed = True
-            recording_status = not recording_status
-            self.logger.info(f"Recording status: {recording_status}")
-        elif ps_controller.cross == 0:
-            cross_pressed = False
-            # self.logger.info("Cross button released")
+#     while self.running:
+#         if ps_controller.cross == 1 and not cross_pressed:
+#             cross_pressed = True
+#             recording_status = not recording_status
+#             self.logger.info(f"Recording status: {recording_status}")
+#         elif ps_controller.cross == 0:
+#             cross_pressed = False
+#             # self.logger.info("Cross button released")
         
-        if len(self.interpolated_lidar_data) > 0:
-            lidar_data = deepcopy(self.interpolated_lidar_data[-1])
-            x, y, rx, ry = ps_controller.get_analog_stick_values()
+#         if len(self.interpolated_lidar_data) > 0:
+#             lidar_data = deepcopy(self.interpolated_lidar_data[-1])
+#             x, y, rx, ry = ps_controller.get_analog_stick_values()
 
-            lidar_data_str = f"LIDAR_DATA#{lidar_data}"
-            analog_sticks_str = f"ANALOG_STICKS#{x}#{y}#{rx}#{ry}"
+#             lidar_data_str = f"LIDAR_DATA#{lidar_data}"
+#             analog_sticks_str = f"ANALOG_STICKS#{x}#{y}#{rx}#{ry}"
 
-            # print(f"Analogdatastr: {analog_sticks_str}")
+#             # print(f"Analogdatastr: {analog_sticks_str}")
              
-            self.client.send_message(lidar_data_str)
-            self.client.send_message(analog_sticks_str)
+#             self.client.send_message(lidar_data_str)
+#             self.client.send_message(analog_sticks_str)
             
-            if recording_status:
-                with open(f"RPIs/DataManager/Data/lidar_data_{file_uuid}_{date}.txt", "a") as file:
-                    file.write(f"{lidar_data}\n")
+#             if recording_status:
+#                 with open(f"RPIs/DataManager/Data/lidar_data_{file_uuid}_{date}.txt", "a") as file:
+#                     file.write(f"{lidar_data}\n")
                     
-                with open(f"RPIs/DataManager/Data/x_values_{file_uuid}_{date}.txt", "a") as file:
-                    file.write(f"{x}\n")
+#                 with open(f"RPIs/DataManager/Data/x_values_{file_uuid}_{date}.txt", "a") as file:
+#                     file.write(f"{x}\n")
         
-        end_time = time.time()
-        print(f"Loop iteration time: {end_time - start_time} seconds")
+#         end_time = time.time()
+#         print(f"Loop iteration time: {end_time - start_time} seconds")
 
-        time.sleep(0.1 - (end_time - start_time))
+#         time.sleep(0.1 - (end_time - start_time))
 
-        start_time = time.time()
+#         start_time = time.time()
--- RPIs/Devices/I2C/Gyro/GyroManager.py
+++ RPIs/Devices/I2C/Gyro/GyroManager.py
@@ -1,7 +1,124 @@
+import threading
+import adafruit_mpu6050
+import busio
+import board
+import time
 # Need to copy files from old repo and enhance them to work nicely. 
 # It would be nice to use the DMP for accurate positioning and connect the interrupt pin for 
 # better performance.
 
 class GyroSensor():
     def __init__(self):
-        pass
+        i2c = busio.I2C(board.D1, board.D0)
+        self.sensor = adafruit_mpu6050.MPU6050(i2c)
+        self.sensor.gyro_range = 0
+        
+        self.angle = 0.0  # Initial angle
+        self.last_time = time.monotonic()
+        self._running = False
+        self._thread = None
+
+    def _get_angle_loop(self):
+        offset_x = 0.29
+        offset_y = 0.0
+        offset_z = 0.0
+
+        while self._running:
+            time.sleep(0.01)
+            
+            # Read gyroscope data and apply offsets
+            gyro_data = self.sensor.gyro
+            gyro_data = [gyro_data[0] + offset_x, gyro_data[1] + offset_y, gyro_data[2] + offset_z]
+            
+            # Get the current time
+            current_time = time.monotonic()
+
+            # Calculate the time elapsed since the last measurement
+            delta_time = current_time - self.last_time
+
+            # Ensure delta_time is within a reasonable range to avoid large jumps
+            if delta_time >= 0.5:
+                delta_time = 0.003
+
+            # Integrate the gyroscope readings to get the change in angle
+            if -0.02 < gyro_data[0] < 0.02:
+                delta_angle = 0
+            else:
+                delta_angle = gyro_data[0] * delta_time * 60
+
+            # Update the angle
+            self.angle += delta_angle
+
+            # Update the last time for the next iteration
+            self.last_time = current_time
+
+    def start(self):
+        if not self._running:
+            self._running = True
+            self._thread = threading.Thread(target=self._get_angle_loop)
+            self._thread.start()
+
+    def stop(self):
+        if self._running:
+            self._running = False
+            self._thread.join()
+            self._thread = None
+
+    def reset_angle(self):
+        self.angle = 0.0
+                
+
+
+
+
+#A class for reading a MPU6050 Gyroscope
+# class Gyroscope2():
+#     def __init__(self):
+#         self.threadStop = 0
+#         self.rotation = [0, 0, 0]
+        
+#         self.mpu = MPU6050.MPU6050(a_bus=0, a_address=0x69)
+#         self.mpu.dmp_initialize()
+#         self.mpu.set_DMP_enabled(True)
+        
+        
+#         self.mpu.set_x_gyro_offset(int(40))
+#         self.mpu.set_y_gyro_offset(int(-17))
+#         self.mpu.set_z_gyro_offset(int(0))
+        
+#         self.mpu.set_x_accel_offset(int(-90))
+#         self.mpu.set_y_accel_offset(int(-665))
+#         self.mpu.set_z_accel_offset(int(-950))
+        
+#     def read(self):
+#         while self.threadStop == 0:
+#             # Assuming you have already initialized and calibrated your MPU6050 as 'mpu'
+#             # Get FIFO buffer data
+#             fifo_count = self.mpu.get_FIFO_count()
+#             if fifo_count < 42:
+#                 continue
+#             fifo_buffer = self.mpu.get_FIFO_bytes(fifo_count)
+            
+#             # Now you can get quaternion data
+#             quat = self.mpu.DMP_get_quaternion(fifo_buffer)
+            
+            
+#             # Get gravity vector
+#             grav = self.mpu.DMP_get_gravity(quat)
+            
+#             if grav.x == 0 or grav.y == 0 or grav.z == 0:
+#                 continue
+            
+            
+#             # Now you can get roll, pitch, yaw
+#             self.rotation = self.mpu.DMP_get_euler_roll_pitch_yaw(quat, grav)
+            
+#             # rotation = mpu.get_rotation()
+#             # print("X: {0:.2f}, Y: {1:.2f}, Z: {2:.2f}".format(rotation[0], rotation[1], rotation[2]))
+#             # acceleration = mpu.get_acceleration()
+#             # print("X: {0:.2f}, Y: {1:.2f}, Z: {2:.2f}".format(acceleration[0], acceleration[1], acceleration[2]))
+            
+#             # Adjust axes for upright robot
+            
+#             # print("Roll: {0:.2f}, Pitch: {1:.2f}, Yaw: {2:.2f}".format(rotation.x, rotation.y, rotation.z))
+   
