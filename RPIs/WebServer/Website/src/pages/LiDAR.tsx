a5c216c180909a4dc20b77d95f055bdeb1990ab6
Emil Deßloch <emildess0303@gymhelm.onmicrosoft.com>
2024-07-28 11:04:06 +0200

    Update LiDAR.tsx

--- RPIs/WebServer/Website/src/pages/LiDAR.tsx
+++ RPIs/WebServer/Website/src/pages/LiDAR.tsx
@@ -1,17 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import LIDARPlot from '../components/LIDAR/LIDARPlot.js';
 
 const LIDAR: React.FC = () => {
-    const [useInterpolated, setUseInterpolated] = useState(false);
-
-    const handleToggle = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUseInterpolated(event.target.value === 'interpolated');
-    };
-
-    const lidarDataURL = useInterpolated
-        ? '/lidar/interpolated_data'
-        : '/lidar/data';
-
     return (
         <div className="bg-white dark:bg-slate-700 text-black dark:text-white">
             <h2 className="text-center text-3xl font-bold p-4">
