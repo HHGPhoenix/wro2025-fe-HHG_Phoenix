1fe1dcfb9267c5dd38cb7e4c94394c69f477ca12
EinEisbxr <FeliSchw1104@gymhelm.onmicrosoft.com>
2024-07-29 17:57:56 +0200

    changes

--- RPIs/AIController/Mainloops/OpeningRace.py
+++ RPIs/AIController/Mainloops/OpeningRace.py
@@ -8,32 +8,37 @@ def main_loop_opening_race(self):
     self.model = tf.keras.models.load_model('RPIs/AIController/model.h5')
     
     while self.running:
-        # run the model
-        if len(self.interpolated_lidar_data) == 0:
-            time.sleep(0.1)
-            continue
-        
-        lidar_data = []
-        
-        # print(f"len(self.lidar_data): {len(self.lidar_data)}, self.lidar_data[-1]: {self.lidar_data}")
-        
-        df = pd.DataFrame(self.interpolated_lidar_data, columns=["angle", "distance", "intensity"])
-        
-        df = df.drop(columns=["intensity"])
+        try:
+            # run the model
+            if len(self.interpolated_lidar_data) == 0:
+                time.sleep(0.1)
+                continue
+            
+            lidar_data = []
+            
+            # print(f"len(self.lidar_data): {len(self.lidar_data)}, self.lidar_data[-1]: {self.lidar_data}")
+            
+            df = pd.DataFrame(self.interpolated_lidar_data, columns=["angle", "distance", "intensity"])
+            
+            df = df.drop(columns=["intensity"])
 
-        df_interpolated_list = df.values.tolist()  
-        
-        lidar_data.append(df_interpolated_list)
-        
-        # print(f"lidar_data: {lidar_data}")
-        
-        prediction = self.model.predict(lidar_data)
-        
-        print(f"prediction: {prediction}")
-        
-        servo_angle = self.servo.mapToServoAngle(prediction[0][0])
-        self.servo.setAngle(servo_angle)
-        
-        motor_speed = 0.3
-        
-        self.motor_controller.send_speed(motor_speed)
+            df_interpolated_list = df.values.tolist()  
+            
+            lidar_data.append(df_interpolated_list)
+            
+            # print(f"lidar_data: {lidar_data}")
+            
+            prediction = self.model.predict(lidar_data)
+            
+            print(f"prediction: {prediction}")
+            
+            servo_angle = self.servo.mapToServoAngle(prediction[0][0])
+            self.servo.setAngle(servo_angle)
+            
+            motor_speed = 0.15
+            
+            self.motor_controller.send_speed(motor_speed)
+            
+        except KeyboardInterrupt:
+            self.motor_controller.send_speed(0)
+            self.running = False
