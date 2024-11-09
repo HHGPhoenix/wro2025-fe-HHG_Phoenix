a2e9a9195767b02ed54f1af202d22fae97428955
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2024-11-09 09:48:04 +0100

    Update oldtonewformat.py

--- AI/dataModifying/oldtonewformat.py
+++ AI/dataModifying/oldtonewformat.py
@@ -1,18 +1,37 @@
 import numpy as np
 import pandas as pd
 
-file_path = r"C:\Users\felix\OneDrive - Helmholtz-Gymnasium\Flix,Emul Ordner\WRO2025\PrototypeV2\15.09.25_Dataset_no_blocks\run_data_51a1f119-f9ba-4f30-96bf-5cc83a89d59a_2024-09-15.npz"
+file_path = r"C:\Users\felix\OneDrive - Helmholtz-Gymnasium\Flix,Emul Ordner\WRO2025\PrototypeV2\15.09.25_Dataset_no_blocks\run_data_b7f2b4a9-78e9-490e-9ff4-14152423a012_2024-09-15.npz"
 
 data = np.load(file_path)
 
-lidar_dataframe = pd.DataFrame(data['lidar_data'])
-lidar_dataframe = lidar_dataframe[(lidar_dataframe["angle"] < 140) | (lidar_dataframe["angle"] > 220)]
-lidar_data = lidar_dataframe.to_numpy()
+controller_data = data['controller_data']
+counters = data['counters']
 
-frame_array = np.array()
-for frame in data['frame_data']:
+new_lidar_data = []
+for lidar_array in data['lidar_data']:
+    angles = lidar_array[:, 0]
+    distances = lidar_array[:, 1]
+    lidar_dataframe = pd.DataFrame({"angle": angles, "distance": distances})
+    lidar_dataframe = lidar_dataframe[(lidar_dataframe["angle"] < 135) | (lidar_dataframe["angle"] > 237)]
+    lidar_data = lidar_dataframe.to_numpy()
+    new_lidar_data.append(lidar_data)
+new_lidar_data = np.array(new_lidar_data)
+
+raw_frame_array = []
+for frame in data['raw_frames']:
     frame = np.frombuffer(frame, dtype=np.uint8).reshape((110, 213, 3))
     frame = frame[10:, :, :]
-    frame_array.append(frame)
+    raw_frame_array.append(frame)
+raw_frame_array = np.array(raw_frame_array)
 
-np.savez(r"C:\Users\felix\OneDrive - Helmholtz-Gymnasium\Flix,Emul Ordner\WRO2025\PrototypeV2\15.09.25_Dataset_no_blocks\run_data_51a1f119-f9ba-4f30-96bf-5cc83a89d59a_2024-09-15.npz", lidar_data=lidar_data, frame_data=frame_array)
+simplified_frame_array = []
+for frame in data['simplified_frames']:
+    frame = np.frombuffer(frame, dtype=np.uint8).reshape((110, 213, 3))
+    frame = frame[10:, :, :]
+    print(frame.shape)
+    simplified_frame_array.append(frame)
+simplified_frame_array = np.array(simplified_frame_array)
+
+np.savez(r"C:\Users\felix\OneDrive - Helmholtz-Gymnasium\Flix,Emul Ordner\WRO2025\PrototypeV2\15.09.25_Dataset_no_blocks\run_data_b7f2b4a9-78e9-490e-9ff4-14152423a012_2024-09-15_new_format.npz", 
+         lidar_data=new_lidar_data, raw_frames=raw_frame_array, simplified_frames=simplified_frame_array, controller_data=controller_data, counters=counters)
