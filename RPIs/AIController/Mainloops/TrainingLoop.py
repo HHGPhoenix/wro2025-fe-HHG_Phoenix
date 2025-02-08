ce4f24093d3c07ed12532e44986cdd155d5a72c1
Felix Schweigmann <84675375+EinEisbxr@users.noreply.github.com>
2025-02-08 09:14:55 +0100

    Update TrainingLoop.py

--- RPIs/AIController/Mainloops/TrainingLoop.py
+++ RPIs/AIController/Mainloops/TrainingLoop.py
@@ -6,6 +6,7 @@ def main_loop_training(self):
         self.logger.info("Starting main loop for training...")
         steer_servo = True
         control_speed = True
+        speed_sent = False
         
         while self.running:
             try:
@@ -19,10 +20,17 @@ def main_loop_training(self):
                 if control_speed:
                     if self.ry < 0.55 and self.ry > 0.45:
                         motor_speed = 0.5
+                        if speed_sent:
+                            self.motor_controller.send_speed(motor_speed)
+                            speed_sent = False
+                        
                     else:
                         motor_speed = 0.70
+                        if not speed_sent:
+                            self.motor_controller.send_speed(motor_speed)
+                            speed_sent = True
+                        
                         # motor_speed = self.ry
-                    self.motor_controller.send_speed(motor_speed)
 
                 stop_time = time.time()
                 time.sleep(max(0, 0.05 - (stop_time - start_time)))
